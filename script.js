19cb83c28355283f4a3ad7bdf1cec4ca48e2bdc0
Jimesosa59 <139372440+Jimesosa59@users.noreply.github.com>
2023-07-21 15:37:10 -0400

    proyecto terminado

--- script.js
+++ script.js
@@ -3,16 +3,35 @@ const ASIGNATURA = document.getElementById("materia")
 const PUNTAJE = document.getElementById("puntuaciones") 
 const GRID = document.getElementById("grid"); 
 
-BOTON.addEventListener("click", ()=>{
-    const ROW = document.createElement("div")
+window.addEventListener('load',()=>{
+    const DATOS = JSON.parse(localStorage.getItem("puntuacion") || "[]")
+    for (const dato of DATOS) {
+        generarRow(dato.materia,dato.puntuacion)
+    }
+})
+BOTON.addEventListener("click", ()=>{    
     let materia = ASIGNATURA.value 
     let puntuacion = PUNTAJE.value
-    console.log(materia,puntuacion)
+    if (materia && puntuacion){
+        const STORAGE= JSON.parse(localStorage.getItem("puntuacion") || "[]")
+        STORAGE.push({materia,puntuacion})
+        localStorage.setItem("puntuacion",JSON.stringify(STORAGE))
+        generarRow (materia, puntuacion)
+
+        ASIGNATURA.value=''
+        PUNTAJE.value=''
+    }
+}) 
+
+function  generarRow (materia, puntuacion){
+    const ROW = document.createElement("div")
+    ROW.classList.add("row")
+    //generar row
     const P = document.createElement('span')
-    P.innerHTML = `${materia} ` 
+    P.innerHTML = materia 
     const P1 = document.createElement('span')
     P1.innerHTML = puntuacion
     ROW.appendChild(P)
     ROW.appendChild(P1)
     GRID.appendChild(ROW)
-}) 
+}
